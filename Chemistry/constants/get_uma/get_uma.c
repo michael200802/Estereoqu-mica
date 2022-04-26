f5f4ea5f8b910cd277e2d8d07d0a6d63eeac44e8
Michu_NEET <reyesespinozamichael@gmail.com>
2022-04-25 19:35:43 -0500

    oops, I forgot to delete these lines.

--- Chemistry/constants/get_uma/get_uma.c
+++ Chemistry/constants/get_uma/get_uma.c
@@ -7,7 +7,7 @@
 
 inline num_t get_uma(atomic_num_t atomic_num)
 {
-    printf("get_uma return value using as argument %hhu: %lf\n",atomic_num,uma_table[atomic_num-1].mass);
+    //printf("get_uma return value using as argument %hhu: %lf\n",atomic_num,uma_table[atomic_num-1].mass);
     if(atomic_num < 1 || atomic_num > NUMBER_OF_ELEMENTS)
     {
         return -1;
